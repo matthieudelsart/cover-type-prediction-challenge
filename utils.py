66c5116a1231330f3cd726f7608d2c2545a24bdb
FrancoisLbrn <francois.lebrun3@gmail.com>
2024-02-21 10:15:08 +0100

    Merge utils.py files into one

--- utils.py
+++ utils.py
@@ -1,14 +1,41 @@
-
 import pandas as pd
-
-df_test = pd.read_csv("test-full.csv")
-df_train = pd.read_csv("train.csv")
+from datetime import datetime
 
 
-def clean_predictor(y_pred, df_test=df_test, df_train=df_train):
-    predictions_df = pd.DataFrame(
-        {'Id': y_pred["Id"], 'Cover_Type': y_pred["Cover_Type"]})
-    # predictions_df['Id'] = range(1, len(df_test) + 1)
+def get_data_train(DATA_PATH=''):
+    return pd.read_csv(DATA_PATH+"train.csv")
+
+
+def get_data_test(DATA_PATH=''):
+    return pd.read_csv(DATA_PATH+"test-full.csv")
+
+
+def csv_for_submission(df: pd.DataFrame, name=''):
+    OUTPUT_PATH = "Output/"
+
+    col_names = df.columns.to_list()
+    assert ('Id' in col_names) and (
+        'Cover_Type' in col_names), "Columns names Id and Cover_Type not found"
+
+    submission = pd.DataFrame({'Id': df.Id, 'Cover_Type': df.Cover_Type})
+    if name != '':
+        file_name = name + ".csv"
+    else:
+        cur_date = datetime.now().strftime("%Y-%m-%d-%H.%M.%S")
+        file_name = f"Submission_{cur_date}.csv"
+    submission.to_csv(OUTPUT_PATH + file_name, index=False)
+
+
+def clean_predictor(y_pred, Id=None):
+    df_test = get_data_test()
+    df_train = get_data_train()
+
+    if Id is not None:
+        predictions_df = pd.DataFrame(
+            {'Id': Id, 'Cover_Type': y_pred})
+    else:   # We assume the prediction are sorted
+        predictions_df = pd.DataFrame({'Cover_Type': y_pred})
+        predictions_df['Id'] = range(1, len(df_test) + 1)
 
     # Removing those in df_train
     predictions_df.drop(predictions_df[predictions_df["Id"].isin(
