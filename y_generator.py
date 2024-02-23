2b30d0404cd9939e1425d3cefafbbd8e5b2bd7ee
matthieudelsart <matthieu.delsart@hec.edu>
2024-02-23 18:23:30 +0100

    conducted a few tests

---
+++ y_generator.py
@@ -0,0 +1,48 @@
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+from imblearn.over_sampling import ADASYN
+from utils import clean_predictor
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+df_test = pd.read_csv("test-full.csv")
+df_train = pd.read_csv("train.csv")
+
+### 1. OVERSAMPLING CLASS 2 AND 1
+ovs_strat = {1: 30_000, 2: 30_000}
+
+# Separating train 
+X_train = df_train.drop(columns=['Cover_Type'], axis=1)
+y_train = df_train['Cover_Type']
+
+# Oversampling
+adasyn = ADASYN(sampling_strategy=ovs_strat)
+X_train_synth, y_train_synth = adasyn.fit_resample(X_train, y_train)
+X_train_synth = pd.DataFrame(X_train_synth, columns=X_train.columns)
+
+### 2. KMEANS
+km_test = KMeans(n_clusters=7, n_init=10, init="k-means++")
+km_test.fit_predict(df_test.loc[:, "Id":"Wilderness_Area4"])
+df_test["kmean_cluster"] = km_test.labels_
+X_train_synth["kmean_cluster"] = km_test.predict(X_train_synth.loc[:, "Id":"Wilderness_Area4"])
+
+### 3. GENERATING
+cat_col = ["kmean_cluster"]
+cols = X_train_synth.drop(columns=['kmean_cluster']).columns
+
+preprocessor = ColumnTransformer([
+        ("cat", OneHotEncoder(handle_unknown="ignore"), cat_col),
+        ("others", "passthrough", cols),
+    ])
+clf = RandomForestClassifier(n_estimators=150)
+pipe = make_pipeline(preprocessor, clf)
+
+pipe.fit(X_train_synth, y_train_synth)
+y_pred = pipe.predict(df_test)
+predictions_df = clean_predictor(y_pred)
+
+# Having it fit the desired format
+predictions_df.to_csv('test_predictions.csv', index=False) 
