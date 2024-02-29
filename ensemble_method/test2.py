fbeede5ffff9c780468db58cb0bf260fe8cb6005
T1m0uss3 <62285023+tim-vlc@users.noreply.github.com>
2024-02-29 18:02:34 +0100

    Try with PCA + original data RF baseline

--- ensemble_method/test2.py
+++ ensemble_method/test2.py
@@ -6,22 +6,23 @@ import matplotlib.colors as mcolors
 import matplotlib.patches as mpatches
 from sklearn.ensemble import RandomForestClassifier
 
-X_test = pd.read_csv('../test-cleaned.csv').to_numpy()
+test = pd.read_csv('../test-cleaned.csv')
 train = pd.read_csv('../train-cleaned.csv')
 
-y_test = pd.read_parquet('../covertype/ground_truth.parquet')['Cover_Type'].to_numpy()
+truth = pd.read_parquet('../covertype/ground_truth.parquet')
+y_test = truth['Cover_Type'].to_numpy()
 
-X_train = train.iloc[:, :-1].to_numpy()
-y_train = train['Cover_Type'].to_numpy()
+X_test_pca = test.to_numpy()
+X_train_pca = train.iloc[:, :-1].to_numpy()
+y_train_pca = train['Cover_Type'].to_numpy()
 
-from umap import UMAP
 from sklearn.decomposition import PCA
-from sklearn.svm import SVC
 
 dim_red = PCA(n_components=5)
-dim_red.fit(X_test)
+dim_red.fit(X_test_pca)
 
-results = dim_red.transform(X_train)
+train_results = dim_red.transform(X_train_pca)
+test_results = dim_red.transform(X_test_pca)
 
 # Customize the plot
 font_size = 14
@@ -36,9 +37,9 @@ ax = fig.add_subplot(111, projection='3d')
 # Assigning random color to each label
 labels = list(train['Cover_Type'].unique())
 color_dict = {label:list(mcolors.TABLEAU_COLORS.keys())[i] for i, label in enumerate(labels)}
-colors = [color_dict[value] for value in list(y_train)]
+colors = [color_dict[value] for value in list(y_train_pca)]
 
-ax.scatter(results[:, 0], results[:, 4], results[:, 2], c=colors, marker='o')
+ax.scatter(train_results[:, 0], train_results[:, 4], train_results[:, 2], c=colors, marker='o')
 
 ax.set_xlabel('Component 1', fontsize=font_size, fontweight=font_weight)
 ax.set_ylabel('Component 2', fontsize=font_size, fontweight=font_weight)
@@ -52,7 +53,15 @@ plt.legend(handles=legend_handles, fontsize=14)
 plt.savefig('3DUMAP.png', dpi=200)
 
 # Make predictions
-red_df = pd.DataFrame(results, columns=[f'PCA{i+1}' for i in range(5)])
+train_pca_df = pd.DataFrame(train_results, columns=[f'PCA{i+1}' for i in range(5)])
+test_pca_df = pd.DataFrame(test_results, columns=[f'PCA{i+1}' for i in range(5)])
+
+train_pca = pd.concat([train, train_pca_df], axis=1)
+test_pca = pd.concat([test, test_pca_df])
+
+X_test = test_pca.to_numpy()
+X_train = train_pca[train_pca.columns.diff(['Cover_Type'])].to_numpy()
+y_train = train['Cover_Type'].to_numpy()
 
 rf = RandomForestClassifier()
 rf.fit(X_train, y_train)
