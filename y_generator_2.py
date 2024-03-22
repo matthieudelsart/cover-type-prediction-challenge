abc4b9e4d4b994f7e3ff6cdc7a0ce05853f21944
matthieudelsart <matthieu.delsart@hec.edu>
2024-03-22 12:10:56 +0100

    added PCA

--- y_generator_2.py
+++ y_generator_2.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.mixture import GaussianMixture
+from sklearn.decomposition import PCA
 
 df_test = pd.read_csv("test-full.csv")
 df_train = pd.read_csv("train.csv")
@@ -36,7 +36,32 @@ svmsmote = SVMSMOTE(sampling_strategy=ovs_strat)
 X_train_synth, y_train_synth = svmsmote.fit_resample(X_train, y_train)
 X_train_synth = pd.DataFrame(X_train_synth, columns=X_train.columns)
 
-### 2. KMEANS - With ID
+### 2. KMEANS 
+
+# With ID
+km_test = KMeans(n_clusters=25, n_init=5, init="k-means++")
+km_test.fit_predict(df_test.loc[:, "Id": "Horizontal_Distance_To_Fire_Points"])
+df_test["kmean_cluster"] = km_test.labels_
+X_train_synth["kmean_cluster"] = km_test.predict(X_train_synth.loc[:, "Id": "Horizontal_Distance_To_Fire_Points"])
+
+# Without ID
+km = KMeans(n_clusters=12, n_init=5, init="k-means++")
+df_test["GMM"] = km.fit_predict(df_test.loc[:, "Elevation": "Horizontal_Distance_To_Fire_Points"])
+X_train_synth["GMM"] = km.predict(X_train_synth.loc[:, "Elevation": "Horizontal_Distance_To_Fire_Points"])
+
+### 3. PCA
+
+# With ID
+pca_1 = PCA(n_components=4)
+pca_cols_1 = ["PCA_1", "PCA_2", "PCA_3", "PCA_4"]
+df_test.loc[:, pca_cols_1] = pca_1.fit_transform(df_test.loc[:, "Id": "Horizontal_Distance_To_Fire_Points"])
+X_train_synth.loc[:, pca_cols_1] = pca_1.transform(X_train_synth.loc[:, "Id": "Horizontal_Distance_To_Fire_Points"])
+
+# Without ID
+pca_2 = PCA(n_components=2)
+pca_cols_2 = ["PCA_5", "PCA_6"]
+df_test.loc[:, pca_cols_2] = pca_2.fit_transform(df_test.loc[:, "Elevation": "Horizontal_Distance_To_Fire_Points"])
+X_train_synth.loc[:, pca_cols_2] = pca_2.transform(X_train_synth.loc[:, "Elevation": "Horizontal_Distance_To_Fire_Points"])
 
 
 ### 4. GENERATING
